91f735dfd2bebc764c6252ce56d32e6f1d19b784
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-02 20:24:01 +0700

    lecture - type

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Your code goes here...");
+function add(a, b) {
+    return a + b;
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Your code goes here...");
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+const number1: number = 5;
+const number2: number = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
