dd19e6a22b5c8d42bdff03dfb6aa82a3d852844c
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-03 16:16:04 +0700

    Update 012_type_function.ts

--- source/012_type_function.ts
+++ source/012_type_function.ts
@@ -2,3 +2,9 @@ const add: Function = (a: number, b:number): number => {
     return a + b;
 }
 add(5, 5);
+
+function sub(a: number, b: number, cb: (num: number) => void): void {
+    const result = a + b;
+
+    cb(result);
+}
