a7f1ff93dd1d2b0afec2c79b14b7da635c7542b5
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-03 10:54:22 +0700

    lecture: object type

---
+++ source/005_object.js
@@ -0,0 +1,10 @@
+var person = {
+    name: "Bike",
+    age: 23
+};
+console.log(person);
+var human = {
+    name: "bike",
+    age: 20
+};
+console.log(human);
---
+++ source/005_object.ts
@@ -0,0 +1,14 @@
+const person: object = {
+    name: "Bike",
+    age: 23
+}
+console.log(person);
+
+const human: {
+    name: string,
+    age: number
+} = {
+    name: "bike",
+    age: 20
+}
+console.log(human);
