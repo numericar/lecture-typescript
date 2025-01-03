9a38407dedb1341760bd765cde77e19b27f0173b
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-03 11:03:24 +0700

    lecture: array

---
+++ source/006_array.js
@@ -0,0 +1,11 @@
+var person = {
+    name: "numerica",
+    age: 23,
+    games: ["War Thunder", "World of Warships"]
+};
+console.log(person);
+var games = ["War Thunder", "World of Warships"];
+for (var _i = 0, games_1 = games; _i < games_1.length; _i++) {
+    var game = games_1[_i];
+    console.log(game);
+}
---
+++ source/006_array.ts
@@ -0,0 +1,11 @@
+const person: object = {
+    name: "numerica",
+    age: 23,
+    games: ["War Thunder", "World of Warships"]
+}
+console.log(person);
+
+const games: string[] = ["War Thunder", "World of Warships"];
+for (const game of games) {
+    console.log(game);
+}
